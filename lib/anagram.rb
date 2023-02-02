aaba24efb8ecfb3600c05358ca2262162a343f09
Rebecca Wollard <rbwollard@gmail.com>
2023-02-02 16:08:53 -0700

    commit tree 254a66ccfcfadfa316f633206365653642272090

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    
+
+    def match(array)
+    array.select do |array|
+        array.chars.sort == word.chars.sort
+        # binding.pry
+    end
+    end
+  
+        
+    
+end  
+      
+
+# binding.pry
+# 0
